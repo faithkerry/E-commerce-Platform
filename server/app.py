f9a6d445b5049657ecb584beba69d375502aaefe
faith-kerry <kerubofaith944@gmail.com>
2026-04-18 00:31:41 +0300

    Add user model with password hashing and create postresql tables

--- server/app.py
+++ server/app.py
@@ -1,37 +1,30 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from dotenv import load_dotenv
 import os
 
-# Load environment variables
+from models import db
+
 load_dotenv()
 
-# Initialize extensions
-db = SQLAlchemy()
 jwt = JWTManager()
 
 
 def create_app():
     app = Flask(__name__)
 
-    # Configuration
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
 
-    # Initialize extensions with app
     db.init_app(app)
     jwt.init_app(app)
 
     @app.route("/")
     def home():
-        return {"message": "E-commerce API running"}
+        return {"message": "API running successfully"}
 
     return app
 
 
 app = create_app()
-
-if __name__ == "__main__":
-    app.run(debug=True)
