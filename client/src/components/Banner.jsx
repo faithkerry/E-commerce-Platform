f42ac1f62df1e19db2c02883a3446a8f98291a51
faith-kerry <kerubofaith944@gmail.com>
2026-04-26 11:46:57 +0300

    Updated Banner,jsx component

---
+++ client/src/components/Banner.jsx
@@ -0,0 +1,21 @@
+function Banner() {
+  return (
+    <div className="bg-gradient-to-r from-blue-800 to-green-500 rounded-2xl m-4 p-8 text-white">
+
+      <h1 className="text-4xl font-bold">
+        Shop Smart,<br /> Live Better
+      </h1>
+
+      <p className="mt-2">
+        Quality products, best prices and fast delivery.
+      </p>
+
+      <button className="mt-4 bg-white text-black px-6 py-2 rounded-full">
+        Shop Now
+      </button>
+
+    </div>
+  );
+}
+
+export default Banner;
